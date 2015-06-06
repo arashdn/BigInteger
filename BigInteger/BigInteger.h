f7f8f46a16a4e6ef71816739a8b760ae0127106e
Arashdn <arash@dargahi.org>
2015-06-06 22:55:14 +0430

    A lot of changes + multiply in new way

--- BigInteger/BigInteger.h
+++ BigInteger/BigInteger.h
@@ -1,17 +1,18 @@
 #pragma once
-#include <stdlib.h>
+#define MULTIPLY_THRESHOLD 4
+#include <ostream>
 #include <string>
 #include <sstream>
 using namespace std;
 
 class BigInteger;//prototype to add friend class in node
 
-template <class T>
 class Node
 {
 private:
-	T value;
+	int value;
 	Node * next;
+	Node * previous;
 
 	friend class BigInteger;
 	friend std::ostream & operator <<(std::ostream& os, BigInteger lst);
@@ -19,32 +20,90 @@ private:
 
 class BigInteger
 {
-
 	friend std::ostream & operator <<(std::ostream& os, BigInteger lst);
+
+private:
+	Node * first;
+	Node * last;
+
+	BigInteger()
+	{
+		first = NULL;
+		last = NULL;
+	}
+
+	void reset()
+	{
+		delete[] first;
+		first = NULL;
+		last = NULL;
+	}
+
+	void add(int value)
+	{
+		if (isEmpty())
+		{
+			first = new Node;
+			first->value = value;
+			first->next = NULL;
+			first->previous = NULL;
+			last = first;
+		}
+		else
+		{
+			Node * temp = first;
+			while (temp->next != NULL)
+				temp = temp->next;
+
+			temp->next = new Node;
+			temp->next->next = NULL;
+			temp->next->previous = temp;
+			temp->next->value = value;
+			last = temp->next;
+		}
+		return;
+	}
+
+	void addToFirst(int value)
+	{
+		if (isEmpty())
+		{
+			first = new Node;
+			first->value = value;
+			first->next = NULL;
+			first->previous = NULL;
+			last = first;
+		}
+		else
+		{
+			Node * temp = new Node;
+			temp->value = value;
+			temp->next = first;
+			temp->previous = NULL;
+			first = temp;
+		}
+		return;
+	}
 
 
 public:
-	BigInteger()
-	{
-		start = NULL;
-		last = NULL;
-	}
+
 	BigInteger(int num)
 	{
-		start = NULL;
+		first = NULL;
 		last = NULL;
 		setNumber(num);
 	}
 	BigInteger(string num)
 	{
-		start = NULL;
+		first = NULL;
 		last = NULL;
 		setNumber(num);
 	}
 
 	bool isEmpty()
 	{
-		if (start == NULL)
+		if (first == NULL)
 		{
 			return true;
 		}
@@ -52,6 +111,52 @@ public:
 		{
 			return false;
 		}
+	}
+
+	int getLenght()
+	{
+		if (isEmpty())
+			return 0;
+
+		int count = 0;
+		Node * temp = first;
+		count++;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+			count++;
+		}
+		return count;
+	}
+
+	bool isZero()
+	{
+		if (isEmpty())
+			return false;
+
+		Node * temp = first;
+		while (temp != NULL)
+		{
+			if (temp->value != 0)
+				return false;
+			temp = temp->next;
+		}
+		return true;
+	}
+
+	int toInteger()
+	{
+		if (isEmpty() || getLenght() > 9)
+			return 0;
+
+		int result = 0;
+		Node * temp = first;
+		while (temp != NULL)
+		{
+			result = result * 10 + temp->value;
+			temp = temp->next;
+		}
+		return result;
 	}
 
 	void setNumber(int num)
@@ -65,148 +170,182 @@ public:
 	{
 		reset();
 		int n = num.length();
-		//chon list 2peyvandi nist , lazeme ke addad ro az akhar b aval dar list negah darim ta moshkeli dar jam va zarb pish nayad
-		for (int i = n-1; i >= 0; i--)
+		for (int i = 0; i < n; i++)
 		{
 			add(num[i] - '0');
 		}
+
+	}
+
+	void operator=(BigInteger &b)
+	{
+		first = NULL;
+		last = NULL;
+		if (!b.isEmpty())
+		{
+			Node * temp = b.first;
+			while (temp != NULL)
+			{
+				add(temp->value);
+				temp = temp->next;
+			}
+		}// if
 	}
 
 	BigInteger operator+(BigInteger num2)
 	{
-		Node<int> * a = start;
-		Node<int> * b = num2.start;
+		Node * a = last;
+		Node * b = num2.last;
 		BigInteger result;
 		int carry = 0;
 		int temp;
 
 		while (a != NULL && b != NULL)
 		{
-			temp = a->value + b->value +carry;
+			temp = a->value + b->value + carry;
 			carry = temp / 10;
-			result.add(temp%10);
-			a = a->next;
-			b = b->next;
+			result.addToFirst(temp % 10);
+			a = a->previous;
+			b = b->previous;
 		}//while
 
 		while (a != NULL)
 		{
 			temp = a->value + carry;
-			carry = temp/10;
-			result.add(temp%10);
-			a = a->next;
+			carry = temp / 10;
+			result.addToFirst(temp % 10);
+			a = a->previous;
 		}//while
 
 		while (b != NULL)
 		{
 			temp = b->value + carry;
-			carry = temp/10;
-			result.add(temp%10);
-			b = b->next;
+			carry = temp / 10;
+			result.addToFirst(temp % 10);
+			b = b->previous;
 		}//while
 
 		if (carry == 1)
 		{
-			result.add(1);
+			result.addToFirst(1);
 		}
 		return result;
 	}
 
-
-	BigInteger operator* (BigInteger num2)
+	BigInteger divideByTen(int pow)
 	{
-		Node<int> * a = start;
-		Node<int> *b ;
+		//age tool n bashe va az aval n-pow ta khoone ro bardarim daghighan mishe number/10^pow
+		int n = getLenght();
 
-		BigInteger result(0);
-		BigInteger temp(0);
-		int level = 0;
-		int carry = 0;
-		int tempProduct;
-		while (a != NULL)
+		if (n <= pow)
 		{
-			temp.reset();
-			b = num2.start;
-			for (int i = 0; i < level; i++)
-			{
-				temp.add(0);
-			}
+			BigInteger result(0);
+			return result;
+		}
 
-			while (b != NULL)
-			{
-				tempProduct = ((a->value)*(b->value))+carry;
-				temp.add(tempProduct % 10);
-				carry = tempProduct / 10;
-				b = b->next;
-			}
-			if (carry != 0)
-			{
-				temp.add(carry);
-				carry = 0;
-			}
-			level++;
-			result = result + temp; //Do we need copy cunstructor???
-			a = a->next;
-
-		}//while a
-
-		
+		Node * temp = first;
+		BigInteger result;
+		for (int i = 0; i < n - pow; i++)
+		{
+			result.add(temp->value);
+			temp = temp->next;
+		}
 		return result;
-	}//opertator *
-
-private:
-	Node<int> * start;
-	Node<int> * last;
-	void reset()
-	{
-		delete[] start;
-		start = NULL;
-		last = NULL;
 	}
-	void add(int value)
+
+	BigInteger multiplyPerTen(int pow)
+	{
+		BigInteger result;
+		result = *this;
+		for (int i = 0; i < pow; i++)
+		{
+			result.add(0);
+		}
+		return result;
+	}
+
+	BigInteger remToTen(int pow)
 	{
 		if (isEmpty())
+			return 0;
+		int i = 0;
+		int n = getLenght();
+		BigInteger result;
+		Node * temp = last;
+		while (i++ < pow && temp != NULL)
 		{
-			addToFirst(value);
-			return;
+			result.addToFirst(temp->value);
+			temp = temp->previous;
 		}
-		else
-		{
-			last->next = new Node<int>;
-			last = last->next;
-			last->value = value;
-			last->next = NULL;
-			return;
-		}
+		return result;
 	}
-	void addToFirst(int value)
-	{
-		start = new Node<int>;
-		start->value = value;
-		start->next = NULL;
-		last = start;
-		return;
-	}
-};
 
+
+	BigInteger operator*(BigInteger num2)
+	{
+		if (isEmpty() || num2.isEmpty() || isZero() || num2.isZero())
+		{
+			BigInteger result(0);
+			return result;
+		}
+
+		int len1 = getLenght(), len2 = num2.getLenght();
+		int n;
+
+		if (len1 > len2)
+			n = len1;
+		else
+			n = len2;
+
+		if (n <= MULTIPLY_THRESHOLD)
+		{
+			int u = toInteger();
+			int v = num2.toInteger();
+			BigInteger result(u*v);
+			return result;
+		}
+
+		int m = n / 2;
+		BigInteger x = divideByTen(m);
+		BigInteger y = remToTen(m);
+		BigInteger w = num2.divideByTen(m);
+		BigInteger z = num2.remToTen(m);
+		cout << "\nx=" << x << "\ty=" << y << "\tw=" << w << "\tz=" << z;
+
+
+		BigInteger result;
+		BigInteger temp1 = (x*w).multiplyPerTen(2 * m);
+		BigInteger temp2 = ((x*z) + (w*y)).multiplyPerTen(m);
+		BigInteger temp3 = (y*z);
+		cout << "\ntemp1 = " << temp1 << endl;
+		cout << "\ntemp2 = " << temp2 << endl;
+		cout << "\ntemp3 = " << temp3 << endl;
+		result = temp1 + temp2;
+		result = result + temp3;
+
+		return result;
+
+	}
+
+
+};
 
 std::ostream & operator <<(std::ostream& os, BigInteger lst)
 {
-	Node<int> * link = lst.start;
+	Node * link = lst.first;
 	if (lst.isEmpty())
+	{
 		os << "No Number";
+		return os;
+	}
+
 	stringstream ss;
 	while (link != NULL)
 	{
 		ss << link->value;
 		link = link->next;
 	}
-	string result = ss.str();
-	int n = result.length();
-	for (int i = n - 1; i >= 0; i--)
-	{
-		os << result[i];
-	}
-	
+	os << ss.str();
+
 	return os;
 }
--- BigInteger/Source.cpp
+++ BigInteger/Source.cpp
@@ -5,13 +5,17 @@ using namespace std;
 
 int main()
 {
-	BigInteger a(123456);
-	BigInteger b(5238);
+	BigInteger a("123456");
+	BigInteger b(52386);
+
 	cout << "a= " << a << endl
 		<< "b= " << b << endl
 		<< "a+b = " << a + b << endl
+		<< "a/10^4 = " << a.divideByTen(4) << endl
+		<< "a*10^4 = " << a.multiplyPerTen(4) << endl
+		<< "a%10^4 = " << a.remToTen(4) << endl
+		<< "(int) a = " << a.toInteger() << endl
 		<< "a*b = " << a*b << endl;
-
 	cin.ignore();
 	cin.get();
 	return 0;
